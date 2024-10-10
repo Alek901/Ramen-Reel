356dfcef497140e2d91a657a5cd0fcd84f87c351
Alek Gonzalez <Magicguys1@icloud.com>
2024-10-10 17:33:08 -0600

    Moved the PageImages
    Moved the Page Images to the Public folder in client and added an import to app.tsx in the src folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RamenBowl from '/PageImages/Ramen-Bowl.png'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +13,7 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={RamenBowl} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
--- client/src/assets/components/Navbar.tsx
+++ client/src/assets/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import auth from '../utils/auth';
 
 const Navbar = () => {
