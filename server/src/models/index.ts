26a6223176bae18a9028a83d8a237d99d1d754d6
amanda forrester <a.soriano92@aol.com>
2024-10-10 19:27:22 -0500

    Update index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,6 +1,10 @@
-import sequelize from '../config/connection.js'
-import { UserFactory } from './user.js';
-
-const User = UserFactory(sequelize);
-
-export { User };
+export interface Anime {
+  id: number;
+  title: string;
+  description: string;
+  genre: string[];
+  episodes: number;
+  status: 'Ongoing' | 'Completed';
+  releaseYear: number;
+  rating: number;
+}
