f70b80eb5e2f4e6027e36cf3af5591b62b07d859
amanda forrester <a.soriano92@aol.com>
2024-10-10 21:36:03 -0500

    Update user-seeds.ts

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -3,7 +3,7 @@ import { User } from '../models/user';
 const seedUserData = [
   { username: 'user1', email: 'user1@example.com', password: 'password1' },
   { username: 'user2', email: 'user2@example.com', password: 'password2' },
-  // Add more user objects as needed
+
 ];
 
 const seedUsers = async (): Promise<void> => {
