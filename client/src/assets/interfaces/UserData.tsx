0f623b7608216c55e59985f012ad7e4d2642b4d1
keshon16 <keshonmckie16@gmail.com>
2024-10-16 00:23:09 -0400

    Updated UserData.tsx

--- client/src/assets/interfaces/UserData.tsx
+++ client/src/assets/interfaces/UserData.tsx
@@ -1,8 +1,26 @@
-export interface UserData {
+export interface User {
   id: number | null;
-  username: string | null;
-  email: string | null;
-  password: string | null;
-  description: string | null;
-  
+  username: string;
+  email: string;
+}
+
+export interface UserPreferences {
+  subtitleLanguage: string;
+  autoplayNextEpisode: boolean;
+  dubPreference: 'sub' | 'dub';
+  preferredGeneres: string;
+}
+
+export interface UserStatictics {
+  favoriteGeneres: string;
+  totalWatchedEpisodes: number;
+  mostWatchGeneres: number;
+  totalminutesWatched: number;
+}
+
+export interface AnimeList {
+name: string;
+description: string;
+catagories: string;
+totalEpisodes: string;
 }
